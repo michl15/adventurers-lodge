45613221ecbefd7594e0e93231c8a737ee6b2008
Michelle Lee <michellelee@Michelles-MBP.lan>
2025-02-03 15:33:12 -0800

    sign out feature

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,23 +1,41 @@
 import React from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useNavigate } from "react-router";
+import { Button } from "react-bootstrap";
 
 const HomePage = () => {
     const auth = getAuth();
+    const navigate = useNavigate();
     
     onAuthStateChanged(auth, (user) => {
         if (user) {
           // User is signed in, see docs for a list of available properties
           // https://firebase.google.com/docs/reference/js/auth.user
           const uid = user.uid;
+          console.log(uid); 
           // ...
         } else {
           // User is signed out
           // ...
+          navigate('/');
         }
       });
 
+      const onSignOutClick = () => {
+        signOut(auth).then(() => {
+          // Sign-out successful.
+          console.log('User signed out');
+        }).catch((error) => {
+          // An error happened.
+          console.error('Error signing out:', error);
+        });
+      }
+
     return (
         <div>
+          <Button onClick={onSignOutClick}>
+            Sign out
+          </Button>
             Homepage
         </div>
     )
