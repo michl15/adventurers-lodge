a37530a18d836095dd612f7d584df8cbb85b02b9
Michelle Lee <michellelee@Michelles-MBP.lan>
2025-02-21 01:02:14 -0800

    adding more fields for character creation

--- src/components/CharacterCreationPage/CharacterCreationPage.jsx
+++ src/components/CharacterCreationPage/CharacterCreationPage.jsx
@@ -1,68 +1,155 @@
-import React, { useEffect, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { push, ref, set } from "firebase/database";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Toast } from "react-bootstrap";
 import { firebaseDatabase } from "../../firebase/firebase";
+import { useFirebaseAuth } from "../../context/FirebaseAuthContext";
+import { useNavigate } from "react-router";
+import styled from "styled-components";
+import { BASE_STATS } from "../../constants/constants";
+
+const StatsRowContainer = styled(Row)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
 const CharacterCreationPage = () => {
     const [charName, setCharName] = useState('');
     const [charClass, setCharClass] = useState('');
-    const [userId, setUserId] = useState('');
+    const [charStats, setCharStats] = useState(BASE_STATS);
+    const [charLvl, setCharLvl] = useState(1);
+    const [validated, setValidated] = useState(false)
 
+    const { user } = useFirebaseAuth();
+    const navigate = useNavigate();
     const [showToast, setShowToast] = useState(false);
     const toggleToast = () => setShowToast(true)
     const toggleToastOff = () => setShowToast(false)
 
-
-    useEffect(() => {
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            setUserId(user.uid)
-    })
-
-    }, []);
-
-
     const onNameChange = (event) => {
+        setValidated(false);
         setCharName(event.target.value);
     }
 
     const onClassChange = (event) => {
+        setValidated(false);
         setCharClass(event.target.value);
     }
 
-    const onSubmit = () => {
-        const database = firebaseDatabase;
-        const charRef = ref(database, 'characters');
+    const onStatChange = (event, label) => {
+        setValidated(false);
+        setCharStats({...charStats, [label]: Number(event.target.value)})
+    }
 
-        const charData = {
-            name: charName,
-            class: charClass,
-            level: 1
+    const onLevelChange = (event) => {
+        setValidated(false);
+        setCharLvl(Number(event.target.value))
+    }
+
+    const onRollStats = () => {
+        let newStatsObj = {}
+        for(const [key] of Object.entries(charStats)) {
+            console.log(key);
+            // generate a random number between 5-18
+            const newStat = Math.floor((Math.random() * 13) + 5);
+            newStatsObj[key] = newStat;
         }
+        setCharStats(newStatsObj);
+    }
 
-        const newCharKey = push(charRef, charData).key;
-        const userRef = ref(database, "users/" + userId + "/characters/" + newCharKey);
+    const onResetStats = () => {
+        setCharStats(BASE_STATS)
 
-        set(userRef, true);
+    }
 
-        toggleToast();
+    const onSubmit = (event) => {
+        const form = event.currentTarget;
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+
+        if (user && form.checkValidity()) {
+            const database = firebaseDatabase;
+            const charRef = ref(database, 'characters');
+
+            const charData = {
+                name: charName,
+                class: charClass,
+                stats: charStats,
+                level: charLvl
+            }
+
+            const newCharKey = push(charRef, charData).key;
+            const userRef = ref(database, "users/" + user?.uid + "/characters/" + newCharKey);
+
+            set(userRef, true).then(() => {
+                toggleToast();
+                navigate(`/characters/${newCharKey}`);
+            }).catch((error) => {
+                console.error(error)
+            });
+        }
+    }
+
+    const renderStatsForm = () => {
+        const statsNames = Object.keys(charStats);
+        return statsNames.map((stat) => {
+            const modifier = Math.floor((charStats[stat] - 10) / 2);
+            return (
+            <Col key={`stats-${stat}`}>
+                <Form.Group>
+                    <Form.Label>{stat.toUpperCase()}</Form.Label>
+                    <Form.Control type="text" onChange={(event) => {onStatChange(event, stat)}} value={charStats[stat]}/>
+                    <Form.Text>
+                        {modifier >= 0 ? `+${modifier}` : `${modifier}`}
+                    </Form.Text>
+                </Form.Group>
+            </Col>
+        )})
     }
 
 
     return (
         <div>
-            <Form>
+            <Container>
+            <Form onSubmit={onSubmit} validated={validated} noValidate>
+                <Row>
                 <Form.Group>
                     <Form.Label>Character Name</Form.Label>
-                    <Form.Control type="text" onChange={onNameChange} value={charName}/>
+                    <Form.Control required type="text" onChange={onNameChange} value={charName}/>
+                    <Form.Control.Feedback type="invalid">
+                        Please enter a name for your character.
+                    </Form.Control.Feedback>
                 </Form.Group>
-                <Form.Group>
-                    <Form.Label>Character Class</Form.Label>
-                    <Form.Control type="text" onChange={onClassChange} value={charClass}/>
-                </Form.Group>
-                <Button onClick={onSubmit}>Submit</Button>
+                </Row>
+                <Row>
+                    <Col sm={8}>
+                        <Form.Group>
+                            <Form.Label>Character Class</Form.Label>
+                            <Form.Control required type="text" onChange={onClassChange} value={charClass}/>
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a class for your character.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Col>
+                    <Col sm={4}>
+                        <Form.Group>
+                            <Form.Label>Level</Form.Label>
+                            <Form.Control required type="text" onChange={onLevelChange} value={charLvl}/>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <StatsRowContainer>
+                    {renderStatsForm()}
+                    <Col>
+                        <Button onClick={onRollStats}>Roll Stats</Button>
+                        <Button onClick={onResetStats}>Reset</Button>
+                    </Col>
+                </StatsRowContainer>
+                <Button type="submit">Create Character!</Button>
                 <Toast show={showToast} onClose={toggleToastOff}>
                     <Toast.Header>
                         <strong className="me-auto">Character Created</strong>
@@ -70,6 +157,7 @@ const CharacterCreationPage = () => {
                     <Toast.Body>Congraulations! You made a character</Toast.Body>
                 </Toast>
             </Form>
+            </Container>
         </div>
 
     )
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+const BASE_STATS = {str: 10, dex: 10, con: 10, int: 10, wis: 10, cha:10}
+
+export { BASE_STATS }
