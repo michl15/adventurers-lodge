21d88fdb7b936af4631eef9af539a21f4a6eab68
Michelle Lee <michellelee@Michelles-MBP.lan>
2025-04-09 14:48:30 -0700

    chore: prettier

--- src/components/CharacterPage/CharacterDetails.tsx
+++ src/components/CharacterPage/CharacterDetails.tsx
@@ -52,7 +52,7 @@ const CharacterDetails = ({ details }: CharacterDetailsProps) => {
                         <h4>Skills</h4>
                         <Proficiencies
                             charLvl={details.level}
-                            onSwitchChange={() => { }}
+                            onSwitchChange={() => {}}
                             charSkills={details.skills || DEFAULT_PROFICIENCIES}
                             editMode={false}
                         />
